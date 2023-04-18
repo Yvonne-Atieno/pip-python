260c68e96d2813ab8a4aaeb5665e74dea08dfc25
Yvonne Atieno <oyvonneatieno@gmail.com>
2023-04-18 15:41:47 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,19 @@
+#Add item to the existing list to the end.
+fruits=['banana','orange','mango']
+fruits.append('apple')
+print(fruits)
+
+#clear all the element from the list.
+people=['Yvonne','Joyce','lucy']
+people.clear()
+print(people)        
+
+#remove probox from the list.
+car =['subaru','toyota','probox','mazda']
+car.remove('probox')
+print(car)
+
+#Insert item of apple between orange and mango.
+fruits =['banana','orange','mango','lemon']
+fruits.insert(2,'apple')
+print(fruits)
